e97328dc74b3373ae1d6b4c75c8b69127c9fd0f7
Adrian Kuhn <adrian.kuhn@airbnb.com>
2018-03-17 12:28:27 -0700

    Remove download url.

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from distutils.core import setup
 setup(
   name = 'omakase',
   packages = ['omakase'],
-  version = '0.1',
+  version = '0.2',
   description = 'My personal functions.',
   author = 'Adrian Kuhn',
   author_email = 'akuhnplus@gmail.com',
   url = 'https://github.com/akuhn/py-omakase',
-  download_url = 'https://github.com/akuhn/py-omakase/archive/0.1.tar.gz',
-  keywords = [],
-  classifiers = [],
 )
