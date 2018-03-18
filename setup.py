9ef6ca3fc45139a75cffa7ae173ebca3d1245c7c
Adrian Kuhn <adrian.kuhn@airbnb.com>
2018-03-17 18:44:32 -0700

    Add dependencies.

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ except:
 setup(
   name = 'omakase',
   packages = ['omakase'],
-  version = '0.5.3',
+  version = '0.5.4',
   description = 'My personal functions.',
   long_description = long_description,
   author = 'Adrian Kuhn',
   author_email = 'akuhnplus@gmail.com',
   url = 'https://github.com/akuhn/py-omakase',
+  install_requires = ['forbiddenfruit'],
 )
